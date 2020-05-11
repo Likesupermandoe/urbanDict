0ba0138c047769646073e1d7c24090efb44d9892
Simon Zeng <57769231+Likesupermandoe@users.noreply.github.com>
2020-05-11 14:08:41 -0400

    Update urbandict.py

--- urbandict.py
+++ urbandict.py
@@ -2,11 +2,20 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+
 url = "http://urbandictionary.com"
 word = sys.argv[1]
+#TODO
+#only taking 1 word searches right now
 searchurl = url + "/define.php?term=" + word
 response = requests.get(searchurl)
 data = response.text
+
 soup = BeautifulSoup(response.text, 'html.parser')
 meaning = soup.find('div', {'class' :'meaning'}).text
+
 print(meaning)
+
+#TODO
+#print examples
+#print multiple definitions
